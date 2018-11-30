e8db3d77ff9e2faacc056a828878e46651bbf5df
Shazada123 <shazada.begum@hotmail.com>
2018-11-30 11:44:32 +0000

    newcommit2
    changed logo
    fixed all images
    added more arrow links

--- js/java.js
+++ js/java.js
@@ -4,5 +4,9 @@ $(function () {
 	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 	});
   $(".movebutton").click(function()
-    {window.scrollTo(0, $(".move").offset().top-30);})
+    {window.scrollTo(0, $(".move").offset().top-150);})
+  $(".movebutton2").click(function()
+      {window.scrollTo(0, $(".move2").offset().top-150);})
+  $(".movebutton3").click(function()
+          {window.scrollTo(0, $(".move3").offset().top-150);})
 });
