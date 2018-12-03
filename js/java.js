8871d3ac88035780a289e9986c68d82ad3a81434
Shazada123 <shazada.begum@hotmail.com>
2018-12-03 11:06:31 +0000

    commit3
    added:
    woof of the day pics and its overlay slides
    social icons and links

--- js/java.js
+++ js/java.js
@@ -9,4 +9,16 @@ $(function () {
       {window.scrollTo(0, $(".move2").offset().top-150);})
   $(".movebutton3").click(function()
           {window.scrollTo(0, $(".move3").offset().top-150);})
+
+  $("#slideshow > div:gt(0)").hide();
+
+  setInterval(function() {
+    $('#slideshow > div:first')
+      .fadeOut(3500)
+      .next()
+      .fadeIn(3500)
+      .end()
+      .appendTo('#slideshow');
+},    3500);
+
 });
